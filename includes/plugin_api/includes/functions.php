7baeb507e61a21ea0ba6157effad3c0572d953b9
Gene Piki <pikigene01@gmail.com>
2023-04-02 03:52:36 +0200

    update plugin functionality for multi tenancy

--- includes/plugin_api/includes/functions.php
+++ includes/plugin_api/includes/functions.php
@@ -1,16 +1,18 @@
 <?php
+include_once('host.php');
 $data = "";
-function custom_field($value, $table_name, $client, $name, $con)
+function custom_field($value, $table_name, $client, $name)
 {
+    $con = multiTenancy($client, $table_name);
 
     $sql = $con->query("SELECT value FROM $table_name WHERE name = '$name' AND belongs = '$client' ");
-//    exit(json_encode(array("status"=>200,"message"=>mysqli_num_rows($sql))));
+    //    exit(json_encode(array("status"=>200,"message"=>mysqli_num_rows($sql))));
     if (mysqli_num_rows($sql) > 0) {
         $con->query("UPDATE $table_name SET value = '$value' WHERE name = '$name' AND belongs = '$client'");
         $dataGet = $con->query("SELECT value FROM $table_name WHERE name = '$name' AND belongs = '$client'");
-        if(mysqli_num_rows($dataGet) > 0){
-            while($row = mysqli_fetch_assoc($dataGet)){
-            $data = $row["value"];
+        if (mysqli_num_rows($dataGet) > 0) {
+            while ($row = mysqli_fetch_assoc($dataGet)) {
+                $data = $row["value"];
             }
         }
         exit(json_encode(array('status' => 200, "message" => "data fetched", $name => $data)));
@@ -18,9 +20,9 @@ function custom_field($value, $table_name, $client, $name, $con)
         $con->query("INSERT INTO $table_name (belongs,name,value) VALUES ('$client','$name','$value')");
         $dataGet = $con->query("SELECT value FROM $table_name WHERE name = '$name' AND belongs = '$client'");
 
-        if(mysqli_num_rows($dataGet) > 0){
-            while($row = mysqli_fetch_assoc($dataGet)){
-            $data = $row["value"];
+        if (mysqli_num_rows($dataGet) > 0) {
+            while ($row = mysqli_fetch_assoc($dataGet)) {
+                $data = $row["value"];
             }
         }
 
--- includes/plugin_api/includes/host.php
+++ includes/plugin_api/includes/host.php
@@ -1,13 +1,63 @@
 <?php
-$host = 'localhost';
-$password = '';
-$user = 'root';
-$db = "wp";
 
-$con = new mysqli("$host","$user","$password","$db");
+function multiTenancyTables($con,$table_name){
 
-if(!$con){
-    exit(json_encode(array("status"=>404,"message"=>"failed to connect to db")));
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        belongs text DEFAULT NULL,
+        name text DEFAULT NULL,
+        value text DEFAULT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+
+        if ($con->query($sql) === TRUE) { //full connection
+    
+        return $con;
+        } else {
+        
+            echo "Error creating table: " . $con->error;
+        }
+
+        return $con;
 }
 
-?>
+function multiTenancy($belongs, $table_name)
+{
+    if(empty($belongs)) die;
+
+
+    $host = 'localhost';
+    $password = '';
+    $user = 'root';
+    $con = "";
+    $prefixed_db = "riskcurb_$belongs";
+    $db = $prefixed_db;
+
+    $con = new mysqli("$host", "$user", "$password");
+
+    if (!$con) {
+        exit(json_encode(array("status" => 404, "message" => "failed to connect to db")));
+    }
+
+    if (mysqli_select_db($con, $db)) {
+
+        $con = new mysqli("$host", "$user", "$password", "$db");
+
+        $con = multiTenancyTables($con,$table_name);
+        return $con;
+
+    } else {
+        $sql = "CREATE DATABASE IF NOT EXISTS $db";
+        if ($con->query($sql) === TRUE) {
+            $con = new mysqli("$host", "$user", "$password", "$db");
+
+            $con = multiTenancyTables($con,$table_name);
+            return $con;
+
+        } else {
+            echo "Error creating database: " . $con->error;
+        }
+    }
+    return $con;
+}
+// $con->close();
