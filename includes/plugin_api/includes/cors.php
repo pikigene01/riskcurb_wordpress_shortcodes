44a75ca98ee7a186f47f13879520728d5c872305
Gene Piki <pikigene01@gmail.com>
2023-04-03 03:16:57 +0200

    update

--- includes/plugin_api/includes/cors.php
+++ includes/plugin_api/includes/cors.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers:  *");
-header('Access-Control-Allow-Methods:  POST');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 ?>
